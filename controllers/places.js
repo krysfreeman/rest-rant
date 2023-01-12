0e3195b9be4704830a7534c1bc6a13b85d75efd9
KrysFreeman <ksfreeman13@gmail.com>
2023-01-11 19:19:08 -0700

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -1,21 +1,10 @@
 const router = require('express').Router()
 const express = require("express");
+const places = require('../models/places.js')
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/edgar-castrejon-J04BD4ysoh8-unsplash.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/remi-remino-E9kVmtiqqGE-unsplash.jpg'
-      }]
+    
       
       res.render('places/index', { places })
 
@@ -28,9 +17,24 @@ router.get('/', (req, res) => {
     res.send(places[req.params.arrayIndex])
   })
   
-  router.post('/',(req, res) => {
-    res.send('post /places stub ')
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
+  
+  
+  
   
   
 module.exports = router
--- views/default.jsx
+++ views/default.jsx
@@ -1,19 +1,37 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
-            <link rel="stylesheet" href="/css/style.css"/>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
-  }
-  
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        ></link>
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
 
-module.exports = Def
+        {html.children}
+      </body>
+    </html>
+  );
+}
+
+module.exports = Def;
