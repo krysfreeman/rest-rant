f0eba04555436071174551cf192d5886812f1706
KrysFreeman <ksfreeman13@gmail.com>
2023-01-11 21:10:16 -0700

    end of day

--- controllers/places.js
+++ controllers/places.js
@@ -8,6 +8,16 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id });
+  }
+});
 
 router.get("/:id", (req, res) => {
   let id = Number(req.params.id);
@@ -19,9 +29,7 @@ router.get("/:id", (req, res) => {
     res.render("places/show", { place: places[id], id });
   }
 });
-router.get("/:id/edit", (req, res) => {
-  res.send("editstubs");
-});
+
 router.post("/", (req, res) => {
   console.log(req.body);
   if (!req.body.pic) {
@@ -47,6 +55,31 @@ router.delete("/:id", (req, res) => {
   } else {
     places.splice(id, 1);
     res.redirect("/places");
+  }
+});
+
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
   }
 });
 
