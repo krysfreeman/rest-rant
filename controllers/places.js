15050f6f2320c26af37a4d44ac640333c16b20b8
KrysFreeman <ksfreeman13@gmail.com>
2023-01-21 13:25:27 -0700

    validatio defaults

--- controllers/places.js
+++ controllers/places.js
@@ -14,15 +14,16 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  if (req.body.pic === "") {
-    req.body.pic = undefined;
-  }
-  if (req.body.city === "") {
-    req.body.city = undefined;
-  }
-  if (req.body.state === "") {
-    req.body.state = undefined;
-  }
+  // if (req.body.pic === "") {
+  //   req.body.pic = undefined;
+  // }
+  // if (req.body.city === "") {
+  //   req.body.city = undefined;
+  // }
+  // if (req.body.state === "") {
+  //   req.body.state = undefined;
+  // }
+  req.body = _.mapValues(req.body, (v) => (v == "" ? undefined : v));
   db.Place.create(req.body)
     .then(() => {
       res.redirect("/places");
