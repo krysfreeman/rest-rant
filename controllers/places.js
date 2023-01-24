87e0a2f73b6ed9fdeb61c1755f2df8f660b381bf
KrysFreeman <ksfreeman13@gmail.com>
2023-01-23 21:40:40 -0700

    comments added

--- controllers/places.js
+++ controllers/places.js
@@ -47,7 +47,9 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
+    .populate("comments")
     .then((place) => {
+      console.log(place.comments);
       res.render("places/show", { place });
     })
     .catch((err) => {
@@ -55,6 +57,7 @@ router.get("/:id", (req, res) => {
       res.render("error404");
     });
 });
+
 
 router.put("/:id", (req, res) => {
   res.send("PUT /places/:id stub");
@@ -67,6 +70,27 @@ router.delete("/:id", (req, res) => {
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
+
+  router.post("/:id/comment", (req, res) => {
+    console.log(req.body);
+    db.Place.findById(req.params.id)
+      .then((place) => {
+        db.Comment.create(req.body)
+          .then((comment) => {
+            place.comments.push(comment.id);
+            place.save().then(() => {
+              res.redirect(`/places/${req.params.id}`);
+            });
+          })
+          .catch((err) => {
+            res.render("error404");
+          });
+      })
+      .catch((err) => {
+        res.render("error404");
+      });
+  });
+
 
 router.post("/:id/rant", (req, res) => {
   res.send("GET /places/:id/rant stub");
