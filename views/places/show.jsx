728539aed0d699522d1dd78341b65123652500b2
KrysFreeman <ksfreeman13@gmail.com>
2023-01-25 20:07:38 -0700

    added stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,11 +5,24 @@ function show(data) {
   let comments = (
     <h3 className="inactive">No comments yet!</h3>
   )
+  let rating = (
+    <h3 className="inactive">Not yet rated</h3>
+  )
    if (data.place.comments.length) {
+let sumRatings = data.place.comments.reduce((tot, c) => {
+  return tot + c.stars;
+}, 0);
+let averageRating = Math.round(sumRatings / data.place.comments.length);
+let stars = "";
+for (let i = 0; i < averageRating; i++) {
+  stars += "⭐";
+}
+rating = <h3>{stars} stars</h3>;
+
      comments = data.place.comments.map((c) => {
        return (
          <div className="border">
-           <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+           <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! ❤️"}</h2>
            <h4>{c.content}</h4>
            <h3>
              <stong>- {c.author}</stong>
@@ -28,7 +41,7 @@ function show(data) {
           Located in {data.place.city}, {data.place.state}
         </h3>
         <h2>Rating</h2>
-        <h4>Not Rated</h4>
+        <h4>{rating}</h4>
         <h2>Description</h2>
         <h3>{data.place.showEstablished()} </h3>
         <h4>Serving {data.place.cuisines}</h4>
