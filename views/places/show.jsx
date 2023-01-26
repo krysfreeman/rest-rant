810a020c9667274f7e11c6e08f6923abf0eefa51
KrysFreeman <ksfreeman13@gmail.com>
2023-01-25 20:33:18 -0700

    edit and deleted fixed

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,36 +2,32 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
-  let comments = (
-    <h3 className="inactive">No comments yet!</h3>
-  )
-  let rating = (
-    <h3 className="inactive">Not yet rated</h3>
-  )
-   if (data.place.comments.length) {
-let sumRatings = data.place.comments.reduce((tot, c) => {
-  return tot + c.stars;
-}, 0);
-let averageRating = Math.round(sumRatings / data.place.comments.length);
-let stars = "";
-for (let i = 0; i < averageRating; i++) {
-  stars += "⭐";
-}
-rating = <h3>{stars} stars</h3>;
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  let rating = <h3 className="inactive">Not yet rated</h3>;
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars;
+    }, 0);
+    let averageRating = Math.round(sumRatings / data.place.comments.length);
+    let stars = "";
+    for (let i = 0; i < averageRating; i++) {
+      stars += "⭐";
+    }
+    rating = <h3>{stars} stars</h3>;
 
-     comments = data.place.comments.map((c) => {
-       return (
-         <div className="border">
-           <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! ❤️"}</h2>
-           <h4>{c.content}</h4>
-           <h3>
-             <stong>- {c.author}</stong>
-           </h3>
-           <h4>Rating: {c.stars}</h4>
-         </div>
-       );
-     });
-   }
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! ❤️"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
   return (
     <Def>
       <main>
@@ -47,10 +43,10 @@ rating = <h3>{stars} stars</h3>;
         <h4>Serving {data.place.cuisines}</h4>
         <h2>Comments</h2>
         <h4>{comments}</h4>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
           Edit
         </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
           <button type="submit" className="btn btn-danger">
             Delete
           </button>
